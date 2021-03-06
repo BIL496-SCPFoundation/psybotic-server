66912fb64adab05d913a466ff0c99d59c2170221
Muhammed Emre Durdu <muhammedemredurdu@gmail.com>
2021-03-06 21:22:45 +0300

    Edited Psychologist model

--- src/main/java/com/scpfoundation/psybotic/server/models/Psychologist.java
+++ src/main/java/com/scpfoundation/psybotic/server/models/Psychologist.java
@@ -9,6 +9,33 @@ public class Psychologist implements BasicModel<String>{
     @Id
     private String id;
     private Timestamp createdDate;
+    private String firstName;
+    private String lastName;
+    private String title;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     public String getId() {
         return id;
