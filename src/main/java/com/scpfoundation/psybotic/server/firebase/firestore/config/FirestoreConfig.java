f2ae52ce97cfee913365a94f91fc05550bfc9cc2
Muhammed Emre Durdu <muhammedemredurdu@gmail.com>
2021-03-09 01:54:28 +0300

    foo

--- src/main/java/com/scpfoundation/psybotic/server/firebase/firestore/config/FirestoreConfig.java
+++ src/main/java/com/scpfoundation/psybotic/server/firebase/firestore/config/FirestoreConfig.java
@@ -3,6 +3,7 @@ package com.scpfoundation.psybotic.server.firebase.firestore.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
+import jdk.incubator.jpackage.internal.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +26,7 @@ public class FirestoreConfig {
     public Firestore getFirestore(@Value("${app.firebase-configuration-file}") String credentialPath) throws IOException {
         InputStream in = ClassLoader.getSystemResourceAsStream(configPath);
         GoogleCredentials credentials = GoogleCredentials.fromStream(in);
+        Log.info("Deneme");
 
         FirestoreOptions options = FirestoreOptions.newBuilder()
                 .setCredentials(credentials).build();
